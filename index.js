79bc3b92f7abcb918a97c6e566fb89f604922b58
princu-567 <rj11bhawani@gmail.com>
2025-01-16 12:18:15 +0530

    victorian

---
+++ index.js
@@ -0,0 +1,43 @@
+const navbarbtn = () => {
+    document.getElementById("name").classList.toggle("show-navbar")
+    document.getElementById("rotates").classList.toggle("rotate40")
+    document.getElementById("transparent").classList.toggle("bg-transparent")
+    document.getElementById("rotates2").classList.toggle("rotate-45")
+    document.body.classList.toggle("overflow-hidden")
+}
+
+$('.slider').slick({
+    dots: true,
+    arrows: false,
+    infinite: true,
+    variableWidth: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: false
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
+                dots: true,
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                dots: true,
+            }
+        }
+    ]
+});
